5b549ba19c80e2a14db7a3290a8fad6a2566bdfd
qetiproject <ketideveloper@gmail.com>
2025-12-14 19:40:54 +0400

    dispatch loginUser

--- src/app/modules/auth-module/pages/login/login.spec.ts
+++ src/app/modules/auth-module/pages/login/login.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { provideRouter } from "@angular/router";
+import * as AuthActions from '@auth-module';
 import { Login } from "@auth-module";
 import { Store } from "@ngrx/store";
 
@@ -15,9 +16,7 @@ describe('Login ', () => {
                 provideRouter([]),
                 {
                     provide: Store,
-                    useValue: {
-                        dispatch: jasmine.createSpy('dispatch')
-                    },
+                    useValue: jasmine.createSpyObj('Store', ['dispatch'])
                 },
             ],
             schemas: [NO_ERRORS_SCHEMA]
@@ -40,26 +39,50 @@ describe('Login ', () => {
 
     it('form should be invalid initially', () => {
         expect(component.form.invalid).toBeTrue()
-    })
+    });
+
     it('submit button should be disabled when form is invalid', () => {
         const el: HTMLElement = fixture.nativeElement;
         const submit = el.querySelector('[data-testid="LoginSubmit"]') as HTMLButtonElement;
         expect(component.form.invalid).toBeTrue();
         expect(submit.disabled).toBeTrue();
 
-    })
-    it('submit button should be enabled when form is valid', () => {
-        component.form.setValue({
-            emailId: 'keti.xecuriani@gmail.com',
-            password: 'Option123!'
-        });
+    });
 
+    it('submit button should be enabled when form is valid', () => {
+         const credentials = {
+            emailId: 'test@mail.com',
+            password: '123456'
+        };
+
+        component.form.setValue(credentials);
         fixture.detectChanges();
-        
+
         const el = fixture.nativeElement;
         const submit = el.querySelector('[data-testid="LoginSubmit"]') as HTMLButtonElement;
         expect(component.form.valid).toBeTrue();
         expect(submit.disabled).toBeFalse();
-    })
+    });
+
+    it('should dispatch loginUser action with correct payload on submit', () => {
+         const credentials = {
+            emailId: 'test@mail.com',
+            password: '123456'
+        };
+
+        component.form.setValue(credentials);
+
+         (component as any).formDir = {
+            resetForm: jasmine.createSpy('resetForm')
+        };
+
+        component.onSubmit();
+
+        const store = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+        expect(store.dispatch).toHaveBeenCalledWith(
+            AuthActions.loginUser({ payload: credentials })
+        );
+
+    });
 
 });
