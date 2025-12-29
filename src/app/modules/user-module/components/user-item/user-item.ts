b89225921ab5933638f41c4984f4f31f1f9090e2
qetiproject <ketideveloper@gmail.com>
2025-12-29 22:43:39 +0400

    user-item

--- src/app/modules/user-module/components/user-item/user-item.ts
+++ src/app/modules/user-module/components/user-item/user-item.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { UserViewModel } from '@user-module';
 
 @Component({
   selector: 'user-item',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './user-item.html',
 })
 export class UserItem {
-
+  @Input({required: true}) user!: UserViewModel;
 }
--- src/app/modules/user-module/pages/users/users.ts
+++ src/app/modules/user-module/pages/users/users.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { UserFacade, UsersViewModel } from '@user-module';
+import { UserFacade, UserItem, UsersViewModel } from '@user-module';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, ],
+  imports: [CommonModule, UserItem],
   templateUrl: './users.html',
 })
 export class Users{
