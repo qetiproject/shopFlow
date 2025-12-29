8511e3e5f7d63a3d35ff22e644e5ab75257cf0f1
qetiproject <ketideveloper@gmail.com>
2025-12-29 21:10:37 +0400

    ClickOutsideDirective

--- src/app/features/index.ts
+++ src/app/features/index.ts
@@ -1,5 +1,5 @@
 export * from './custom-form/index';
+export * from './directives/click-outsides.directive';
 export * from './directives/message.directive';
 export * from './loading/loading.component';
 export * from './loading/skip-loading.component';
-
