bcf6c70d63b6f16075acda7bea88befd8c843b39
qetiproject <ketideveloper@gmail.com>
2026-02-28 11:21:47 +0400

    signal form

---
+++ src/app/signal-form/signal-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-form',
+  standalone: true,
+  imports: [],
+  template: ``,
+})
+export class SignalFormComponent {}
