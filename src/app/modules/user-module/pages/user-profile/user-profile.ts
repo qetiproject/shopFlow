280eaa6c00d0f20d49f588df0928fa4ee3798fbd
qetiproject <ketideveloper@gmail.com>
2026-03-03 12:33:40 +0400

    uer profile clearer markup

--- src/app/modules/user-module/pages/user-profile/user-profile.ts
+++ src/app/modules/user-module/pages/user-profile/user-profile.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { toSignal } from "@angular/core/rxjs-interop";
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 
@@ -10,10 +10,13 @@ import { map } from 'rxjs';
   templateUrl: './user-profile.html',
 })
 export class UserProfile {
-  #route = inject(ActivatedRoute)
-  userProfile = toSignal(
-    this.#route.data.pipe(map((d) => d['user'])),
-    { initialValue: null }
-  );
-  
+  #route = inject(ActivatedRoute);
+  userProfile = toSignal(this.#route.data.pipe(map((d) => d['user'])), { initialValue: null });
+
+  readonly initials = computed(() => {
+    const fullName = this.userProfile().fullName || '';
+    const info = fullName.trim().split(' ');
+    const firstLetters = info.map((name: string) => name[0].toUpperCase());
+    return firstLetters.slice(0, 2).join('');
+  });
 }
