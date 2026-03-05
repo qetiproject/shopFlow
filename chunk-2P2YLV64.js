1a4a4948d8c0bfc90be29d9c40203dde53053abf
qetiproject <ketideveloper@gmail.com>
2026-03-05 16:22:11 +0400

    removed files

--- chunk-2P2YLV64.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c,d,e,f,g,h,i,j}from"./chunk-TY3QMXIM.js";import"./chunk-W5D7ETMH.js";import"./chunk-XT5ZBVVT.js";export{d as CartComponent,e as CartFacade,b as CartProductItem,f as CartStorage,g as CartStore,c as CartSummary,h as addOrUpdateProduct,j as calculateTotals,a as cartRoutes,i as updateQuantity};
