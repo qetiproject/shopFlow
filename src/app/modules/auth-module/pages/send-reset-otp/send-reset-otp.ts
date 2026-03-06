c063f3ec90faf14b7b19b3609f85150106100abc
qetiproject <ketideveloper@gmail.com>
2026-03-06 21:11:22 +0400

    auth facade fixed

--- src/app/modules/auth-module/pages/send-reset-otp/send-reset-otp.ts
+++ src/app/modules/auth-module/pages/send-reset-otp/send-reset-otp.ts
@@ -1,11 +1,13 @@
-
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthFacade } from '@auth-module/services/auth.facade';
 import { DynamicValidatorMessage } from '@features/custom-form/validators/dynamic-validator-message.directive';
 import { InputComponent } from '@features/custom-form/input/input';
-import { INPUT_TYPES } from '@types';
+import { MessagesService } from '@core/services/messages.service';
+import { INPUT_TYPES, MessageSeverity } from '@types';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-send-reset-otp',
@@ -15,18 +17,33 @@ import { INPUT_TYPES } from '@types';
     FormsModule,
     ReactiveFormsModule,
     InputComponent,
-    DynamicValidatorMessage
-],
+    DynamicValidatorMessage,
+  ],
   templateUrl: './send-reset-otp.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SendResetOtp {
-  INPUT_TYPES = INPUT_TYPES;
-  #authFacade = inject(AuthFacade);
+  readonly INPUT_TYPES = INPUT_TYPES;
+  readonly #authFacade = inject(AuthFacade);
+  readonly #destroyRef = inject(DestroyRef);
+  readonly #messages = inject(MessagesService);
+  readonly #router = inject(Router);
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
-  onSubmit() {
-    this.#authFacade.sendPasswordResetOtp(this.email.value as string);
+  onSubmit(): void {
+    this.#authFacade
+      .sendPasswordResetOtp(this.email.value as string)
+      .pipe(
+        tap((response) => {
+          this.#messages.showMessage({
+            text: response.message,
+            severity: MessageSeverity.Success,
+          });
+          this.#router.navigate(['/reset-password']);
+        }),
+        takeUntilDestroyed(this.#destroyRef),
+      )
+      .subscribe();
   }
 }
